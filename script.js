7ffa83db60639dd1f22ee76521bb27a435fd87a3
Al Zaplin <zaplin.dev@gmail.com>
2024-07-22 20:13:30 +0300

    upd(styles)

--- script.js
+++ script.js
@@ -2,34 +2,38 @@
 const ctx = document.getElementById('myChart');
 
 const data = {
-	labels: [
-		'Done',
-		'Lessons & HWs',
-		'Exam Project'
-	],
-	datasets: [{
-		data: ['78', '12', '10'],
-		backgroundColor: [
-			'rgb(149, 242, 217)',
-			'rgb(237, 177, 131)',
-			'rgb(168, 218, 220)'
-		],
-		hoverOffset: 4
-	}]
+  labels: [
+    'Done',
+    'Lessons & HWs',
+    'Exam Project'
+  ],
+  datasets: [{
+    data: ['78', '12', '10'],
+    backgroundColor: [
+      'rgb(149, 242, 217)',
+      'rgb(237, 177, 131)',
+      'rgb(168, 218, 220)'
+    ],
+    hoverOffset: 4
+  }]
 };
 
 new Chart(ctx, {
 	type: 'pie',
 	data: data,
-	plugins: {
+	options: {
+		layout: {
+				padding: {
+					top: 50
+				}
+		},
 		plugins: {
 			legend: {
 				title: {
-					padding: {
-						top: 50
-					}
+					padding: 200
 				}
 			}
 		}
 	}
 });
+// options.plugins.legend.title
